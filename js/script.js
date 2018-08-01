bb0b4978b7f82cd219b583d15280b8318db4cce8
Matthew Kruk <matthew.kruk@gmail.com>
2018-08-01 12:56:19 -0400

    fixed reservation mouseover display

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $(document).ready (() => {
 
     addTables();
     $(".modal_form").hide();
+    $(".display_reserved").hide();
 
     $(document).on("click", ".availible", (e) => {
         currentTable = $(e.target);
@@ -15,29 +16,37 @@ $(document).ready (() => {
     });
 
     $(document).on("click", "#save, #close", (e) => {
-        $(".modal_form").fadeOut(500);
+        $(".modal_form").fadeOut();
         if ($(e.target).attr("id") === "save") {
             reservationList.push(new Reservation($("#name").val(), $("#phone").val(), $("#no_guests").val(), currentTable.text()));
             currentTable.toggleClass("availible reserved");
         }
     });
 
-    $(document).on("mouseenter mouseout", ".tables .reserved", (e) => {
+    $(document).on("mouseenter", ".tables .reserved", (e) => {
         for(let i = 0; i < reservationList.length; i++) {
-            if ($(e.target).text() === reservationList[i].table) {
-                $(e.target).prepend(`<section class="display_reserved"></section>`);
-                $(".display_reserved").text(`Name: ${reservationList[i].name} Size of Party: ${reservationList[i].no_guests}`);
-            } else {
-                $(".display_reserved").hide();
+            if ($(e.target).attr("id") === reservationList[i].table) {
+                $(".display_reserved")
+                .find("p:first-child")
+                .text(`Name: ${reservationList[i].name}`);
+                $(".display_reserved")
+                .find("p:nth-child(2)")
+                .text(`Size of Party: ${reservationList[i].no_guests}`);
+                $(".display_reserved").appendTo($(e.target));
+                console.log();
+                //$(e.target).append();
+                $(".display_reserved").fadeIn();
             }
         }
+    }).on("mouseleave", ".tables .reserved", (e)=> {
+        $(".display_reserved").fadeOut();
     });
 
 
 
     function addTables () {
         for (let i = 1; i < 10; i++) {
-            $(".tables").append(`<section class="availible">${i}</section>`);
+            $(".tables").append(`<section class="availible" id="${i}" >${i}</section>`);
         }
     }
 
